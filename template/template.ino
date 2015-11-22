ebabb0c751a1dc35d0622c037bc5fe2a4c4fb12c
Chris 'Sembazuru' Elliott <SembazuruCDE@GMail.com>
2015-11-22 11:59:33 -0500

    Changes to template.ino:  * Update default serial baudrate to highest in IDEv1.6.6 (250000)  * Add DebugUtils.h based on Arduino.cc forum thread http://forum.arduino.cc/index.php?topic=46900.0

--- template/template.ino
+++ template/template.ino
@@ -5,6 +5,8 @@
  * date
  */
 
+#define DEBUG
+#include "DebugUtils.h"
 // Add any requred #include lines here for external libraries.
 
 // Define any desired global scope constants and variables.
@@ -12,12 +14,17 @@
 
 void setup()
 {
-  Serial.begin(115200); // Change this to whatever your like running your Serial Monitor at.
+  Serial.begin(250000); // Change this to whatever your like running your Serial Monitor at.
   while (!Serial); // Wait for serial port to connect. Needed for Leonardo only.
   delay(1000); // Simply to allow time for the ERW versions of the IDE time to automagically open the Serial Monitor. 1 second chosen arbitrarily.
-  Serial.println(F("sketchName.ino"));  // so we know what sketch is running
+  Serial.print(F("Sketch Name: "));
+  Serial.println(F(__FILE__));  // so we know what sketch is running
   Serial.println();
-
+#ifdef DEBUG // If project doens't normally use the serial port, move this ifdef line to before the Serial.begin.
+  Serial.println(F("Debug line format-> milliseconds: __PRETTY_FUNCTION__ __FILE__:__LINE__ message"));
+#endif
+  DEBUG_PRINT(F("Debug is on"));
+  
   // Put your setup code here, to run once:
 
 }
